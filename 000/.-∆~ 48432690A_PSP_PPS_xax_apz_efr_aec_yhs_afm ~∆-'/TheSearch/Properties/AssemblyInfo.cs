efc26f60aa85674c7d3c8ba1cf96777c98951b94
ASfullmine <thetrueisouthere@outlook.com>
2025-07-24 08:40:31 +0800

    unas palabras de uno que pasaba por aquí

---
+++ 000/.-∆~ 48432690A_PSP_PPS_xax_apz_efr_aec_yhs_afm ~∆-'/TheSearch/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TheSearch")]
+[assembly: AssemblyDescription("The Search")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("n0n €hhh")]
+[assembly: AssemblyProduct("TheSearch")]
+[assembly: AssemblyCopyright("© Copyright © 1984 © SPAIN © Internet © Phones © Radio & T.V. © Professional Studios © presents.. Casa/s ajena/s non profits, only for homes, business, shops, education programs, knowlegde, technology or religion/s added to The Richs World Conformance or understanding. ©")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
